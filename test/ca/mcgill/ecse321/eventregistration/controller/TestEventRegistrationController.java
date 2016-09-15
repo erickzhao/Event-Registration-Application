bb631bc21b369c90b7d12e096913aa706015ecb8
erick <erick@hotmail.ca>
2016-09-15 08:41:49 -0400

    Track missing files

---
+++ test/ca/mcgill/ecse321/eventregistration/controller/TestEventRegistrationController.java
@@ -0,0 +1,73 @@
+package ca.mcgill.ecse321.eventregistration.controller;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import ca.mcgill.ecse321.eventregistration.controller.EventRegistrationController;
+import ca.mcgill.ecse321.eventregistration.model.Event;
+import ca.mcgill.ecse321.eventregistration.model.Participant;
+import ca.mcgill.ecse321.eventregistration.model.Registration;
+import ca.mcgill.ecse321.eventregistration.model.RegistrationManager;
+import ca.mcgill.ecse321.eventregistration.persistence.PersistenceXStream;
+
+public class TestEventRegistrationController {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		PersistenceXStream.setFilename("test"+File.separator+"ca"+File.separator+"mcgill"+File.separator+"ecse321"+File.separator+"eventregistration"+File.separator+"controller"+File.separator+"data.xml");
+		PersistenceXStream.setAlias("event",Event.class);
+		PersistenceXStream.setAlias("participant", Participant.class);
+		PersistenceXStream.setAlias("registration", Registration.class);
+		PersistenceXStream.setAlias("manager",RegistrationManager.class);
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		//clear all registrations
+		RegistrationManager rm = RegistrationManager.getInstance();
+		rm.delete();
+	}
+
+	@Test
+	public void testCreateParticipant() {
+		RegistrationManager rm = RegistrationManager.getInstance();
+		assertEquals(0, rm.getParticipants().size());
+		
+		String name = "Oscar";
+		
+		EventRegistrationController erc = new EventRegistrationController();
+		erc.createParticipant(name);
+		
+		checkResultParticipant(name, rm);
+		
+		RegistrationManager rm2 = (RegistrationManager)PersistenceXStream.loadFromXMLwithXStream();
+		
+		//check file contents
+		checkResultParticipant(name, rm2);
+		
+		
+	}
+
+	private void checkResultParticipant(String name, RegistrationManager rm) {
+		assertEquals(1, rm.getParticipants().size());
+		assertEquals(name, rm.getParticipant(0).getName());
+		assertEquals(0, rm.getEvents().size());
+		assertEquals(0, rm.getRegistrations().size());
+	}
+
+}
